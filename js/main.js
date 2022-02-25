d113a60603c5656ca9424aad6d165fde0994671e
lucaperini <luca.perini@gmail.com>
2022-02-25 23:09:19 +0100

    aggiunto gioco dadi

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 //Accesso via email
 let registeredUsers = ["luigi@mail.it", "pippo@fastmail.it", "giacomo@miamail.it", "maria@mail.it", "carmine@fastmail.it", "pierluigi@miamail.it", "nazzareno@mail.it","giovanni@fastmail.it", "rita@miamail.it", "gianna@gaetano.it"];
-
-const accessButton=document.querySelector('#login-btn');
+const accessButton = document.querySelector('#login-btn');
 accessButton.addEventListener('click', function() {
 const userEmail=document.getElementById('email-address').value;
 let emailFound = false;
 for (let i = 0; i < registeredUsers.length; i++){
     if (userEmail.value == registeredUsers[i].value){
-        emailFound=true;
+        emailFound == true;
     } 
-    
 }
 if (emailFound == true){
     document.getElementById('welcomePass').innerHTML="Benvenuto nel Fight Club!";
@@ -18,6 +16,24 @@ else {
     document.getElementById('welcomePass').innerHTML="Mi dispiace, non risulti iscritto al nostro club"; 
 }
 });
+
+//Gioca a dadi con il computer
+let playerOne = Math.round(Math.random()*5 + 1);
+    document.getElementById('player-one').innerHTML=playerOne;
+    let playerPc = Math.round(Math.random()*5 + 1);
+    document.getElementById('player-pc').innerHTML=playerPc;
+    if (playerOne > playerPc){
+        document.getElementById('result').innerHTML="Hai vinto tu!";
+    } else if (playerOne < playerPc) {
+        document.getElementById('result').innerHTML="Ha vinto il computer!";
+    }else {
+        document.getElementById('result').innerHTML="Siete pari!";
+}
+// const playButton = document.getElementById('play-btn');
+// playButton.addEventListener('Click', function () {
+    
+    
+// });
 
 
 
